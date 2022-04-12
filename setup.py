33acf21d10c9fc6a6a55341cd7e151576127362d
Sun Lingfeng, Rise Sun <sunlingfeng70@gmail.com>
2022-04-12 10:11:04 +0800

    second

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
        name="flitton_py",
        version="0.0.1",
        author="Maxwell Flitton",
        author_email="maxwell@gmail.com",
        description="Calculates a Fibonacci number",
-       long_description="A basic library that calculates Fibonacci numbers",
+       long_description=long_description,
        long_description_content_type="text/markdown",
     url="https://github.com/minikiller/flitton-py",
     install_requires=[],
